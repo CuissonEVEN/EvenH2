1c6bdf1b02ce8dee8743a63e47df1b970369d98b
chhue <chhue96@gmail.com>
2024-10-22 19:49:48 +0900

    Initial Commit

---
+++ EvenH2Tests/EvenH2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EvenH2Tests.swift
+//  EvenH2Tests
+//
+//  Created by CHYUN on 10/22/24.
+//
+
+import Testing
+@testable import EvenH2
+
+struct EvenH2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
