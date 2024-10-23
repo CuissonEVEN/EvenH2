51c4a281bda5cd25fb7b1e6243a99f7366168e76
sy0201 <noey0201@gmail.com>
2024-10-23 15:28:37 +0900

    [modify] hashTagCollectionVIewCell UI 수정

--- EvenH2/MemberScreen/HashTagCollectionViewCell.swift
+++ EvenH2/MemberScreen/HashTagCollectionViewCell.swift
@@ -10,13 +10,20 @@ import UIKit
 final class HashTagCollectionViewCell: UICollectionViewCell {
     static let id = "HashTagCollectionViewCell"
     
+    @IBOutlet weak var hashtagView: UIView!
     @IBOutlet weak var hashtagLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        hashtagView.backgroundColor = .w100
+        hashtagView.layer.cornerRadius = 12
+        hashtagView.layer.borderColor = UIColor.lightGray.cgColor
+        hashtagView.layer.borderWidth = 1
     }
 
-    func configure(with hashtag: String) {
-        hashtagLabel.text = hashtag
+    func configure(with hashtagText: String) {
+        hashtagLabel.textColor = UIColor.darkGray
+        hashtagLabel.font = UIFont.dmSans(size: 15, weight: .black)
+        hashtagLabel.text = hashtagText
     }
 }
