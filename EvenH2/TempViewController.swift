5e09c0cd106cba77b02cfb169bbcd6e806a4d89f
chhue <chhue96@gmail.com>
2024-10-23 11:05:46 +0900

    [feat] 테스트화면 이동을 위한 버튼 추가 (버튼 이동 메소드 내용 추가 필요)

---
+++ EvenH2/TempViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TempViewController.swift
+//  EvenH2
+//
+//  Created by CHYUN on 10/23/24.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+    @IBOutlet weak var memberButton: UIButton!
+    @IBOutlet weak var memDetailButton: UIButton!
+    @IBOutlet weak var introductionButton: UIButton!
+    @IBOutlet weak var goalButton: UIButton!
+    @IBOutlet weak var promiseButton: UIButton!
+    @IBOutlet weak var mainButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func geMember(_ sender: UIButton) {
+
+    }
+    
+    
+    @IBAction func goMemDetail(_ sender: UIButton) {
+
+    }
+    
+    
+    @IBAction func goIntroduction(_ sender: UIButton) {
+    }
+    
+    @IBAction func goGoal(_ sender: UIButton) {
+    }
+    
+    @IBAction func goPromise(_ sender: UIButton) {
+    }
+    
+    @IBAction func goMain(_ sender: UIButton) {
+    }
+
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
