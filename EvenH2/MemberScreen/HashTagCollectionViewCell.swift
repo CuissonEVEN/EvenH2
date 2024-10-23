a16bb32ac616f762d6e9a95994c415517efb35ba
sy0201 <noey0201@gmail.com>
2024-10-23 10:08:47 +0900

    [feat] MemberDetailVC 중 Section 0번째 hashtagCell 추가

---
+++ EvenH2/MemberScreen/HashTagCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemberDetailCollectionViewCell.swift
+//  EvenH2
+//
+//  Created by miranda on 10/23/24.
+//
+
+import UIKit
+
+class MemberDetailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
