a960a77235fb02337d7d6b09f446745d04a97804
DoyleHWorks <DoyleHWorks@gmail.com>
2024-10-23 15:56:56 +0900

    [modify] 런치스크린 지연시간 조정

--- EvenH2/AppDelegate.swift
+++ EvenH2/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        sleep(1) // Launch Screen 지연시간
+        sleep(2) // Launch Screen 지연시간
         return true
     }
 
