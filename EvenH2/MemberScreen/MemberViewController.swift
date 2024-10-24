24cfe6d19988cbd739d0fcc6fed302b6ca033ce4
chhue <chhue96@gmail.com>
2024-10-24 14:37:17 +0900

    [feat] Members struct 만들어서 데이터 추가

--- EvenH2/MemberScreen/MemberViewController.swift
+++ EvenH2/MemberScreen/MemberViewController.swift
@@ -8,16 +8,81 @@
 
 import UIKit
 
+struct EvenMember {
+    private var key: Int
+    private var enName: String
+    private var krName: String
+    private var tmi: String
+    private var imgAssetName: String
+    private var hash: [String]
+    private var introduction: String
+    private var role: String
+    private var blogUrlPath: String
+    private var gitUrlPath: String
+    
+    init(key: Int, enName: String, krName: String, tmi: String, imgAssetName: String, hash: [String], introduction: String, role: String, blogUrlPath: String, gitUrlPath: String) {
+        self.key = key
+        self.enName = enName
+        self.krName = krName
+        self.tmi = tmi
+        self.imgAssetName = imgAssetName
+        self.hash = hash
+        self.introduction = introduction
+        self.role = role
+        self.blogUrlPath = blogUrlPath
+        self.gitUrlPath = gitUrlPath
+    }
+    
+    func getKey() -> Int { return key }
+    func getEnName() -> String { return enName }
+    func getKrName() -> String { return krName }
+    func getTmi() -> String { return tmi }
+    func getImgAssetName() -> String { return imgAssetName }
+    func getHash() -> [String] { return hash }
+    func getIntroduction() -> String { return introduction }
+    func getRole() -> String { return role }
+    func getBlogUrlPath() -> String { return blogUrlPath }
+    func getGitUrlPath() -> String { return gitUrlPath }
+    
+}
+
+
 class MemberViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     
-    let members: [(String,String)] = [
-        ("Chae-Hyun Park", "동생이 지난 달에 전역함 👽"),
-        ("Si-Yeon Park", "집 앞 떡볶이 JMT 💕"),
-        ("Doyle Hwang", "커피보다는 마테차 🍵"),
-        ("Min-Seok Park", "아직 여름이불 덮는 중~ 💦"),
-        ("Young-Jin Na", "창문이 너무 커서 뜨거움 🌈"),
-        ("Sang-Kyeong Jang", "애플워치 시리즈10 구매 😈")
+//    let members: [(String,String)] = [
+//        ("Chae-Hyun Park", "동생이 지난 달에 전역함 👽"),
+//        ("Si-Yeon Park", "집 앞 떡볶이 JMT 💕"),
+//        ("Doyle Hwang", "커피보다는 마테차 🍵"),
+//        ("Min-Seok Park", "아직 여름이불 덮는 중~ 💦"),
+//        ("Young-Jin Na", "창문이 너무 커서 뜨거움 🌈"),
+//        ("Sang-Kyeong Jang", "애플워치 시리즈10 구매 😈")
+//    ]
+    
+    let members: [EvenMember] = [
+        EvenMember(key: 0, enName: "Chae-Hyun Park", krName: "박채현", tmi: "동생이 지난 달에 전역함 👽", imgAssetName: "img_ch", hash: ["ESTJ", "적극적", "리더십", "KIA팬"],
+                   introduction: "안녕하세요, 저는 2조의 팀장을 맡고 있는 박채현입니다.  팀원분들의 의견을 적극 수용하려 노력하며 작업을 진행하였습니다. 2븐조 팀원이 제일 최고야!", role: "헤드 프레젠터",
+                   blogUrlPath: "https://chhue96.tistory.com/", gitUrlPath: "https://github.com/chaehyunp"),
+        
+        EvenMember(key: 1, enName: "Si-Yeon Park", krName: "박시연", tmi: "집 앞 떡볶이 JMT 💕", imgAssetName: "img_sy", hash: ["ISFJ", "바지런함", "부드러움", "명예소방관"],
+                   introduction: "안녕하세요 2조 박시연입니다. 이번 소개 앱에서는 팀원 디테일 뷰를 맡았습니다. 멘탈케어와 간단한 코드 이슈를 함께 해결했습니다!", role: "미니 튜터",
+                   blogUrlPath: "https://velog.io/@sy0201", gitUrlPath: "https://github.com/sy0201"),
+        
+        EvenMember(key: 2, enName: "Doyle Hwang", krName: "황도일", tmi: "커피보다는 마테차 🍵", imgAssetName: "img_dy", hash: ["INFP", "침착함", "끈기", "포토그래퍼"],
+                   introduction: "안녕하세요! 2조의 깐죽 담당 황도일입니다. 놓치기 쉬운 디테일을 보고 완성도를 높이기 위해 팀원을 귀찮게 하는 걸 좋아합니다 >:D", role: "세션 리마인더",
+                   blogUrlPath: "https://velog.io/@doylehworks", gitUrlPath: "https://github.com/DoyleHWorks"),
+        
+        EvenMember(key: 3, enName: "Min-Seok Park", krName: "박민석", tmi: "아직 여름이불 덮는 중~ 💦", imgAssetName: "img_ms", hash: ["INTP", "분석적", "섬세함", "Git해결사"],
+                   introduction: "안녕하세요, 저는 2조의 팀장을 맡고 있는 박채현입니다.  팀원분들의 의견을 적극 수용하려 노력하며 작업을 진행하였습니다. 2븐조 팀원이 제일 최고야!", role: "바이브 매니저",
+                   blogUrlPath: "https://iosstudyletsgo.tistory.com/", gitUrlPath: "https://github.com/maxminseok"),
+        
+        EvenMember(key: 4, enName: "Young-Jin Na", krName: "나영진", tmi: "창문이 너무 커서 뜨거움 🌈", imgAssetName: "img_yj", hash: ["INFJ", "차분함", "말 잘들음", "산들바람"],
+                   introduction: "안녕하세요 2조 나영진입니다. 약속 뷰를 맡았습니다.  개발이 처음이지만 팀원분들의 많은 도움 덕분에 재미도 느끼고 잘 따라갈 수 있었습니다.", role: "디지털 전서구",
+                   blogUrlPath: "https://yjuni22.tistory.com/", gitUrlPath: "https://github.com/bryjna07"),
+        
+        EvenMember(key: 5, enName: "Sang-Kyeong Jang", krName: "장상경", tmi: "애플워치 시리즈10 구매 😈", imgAssetName: "img_sk", hash: ["INFJ", "책임감", "검색능력", "귀여움은정의"],
+                   introduction: "안녕하세요, 저는 2조에서 부발표자를 맡고 있는 장상경입니다. 부족하지만 팀원분들과 최대한 협력하여 좋은 결과물이 나올 수 있도록 이끌고 따라가도록 하겠습니다.", role: "프레젠트 코디네이터",
+                   blogUrlPath: "https://velog.io/@crois0509/posts", gitUrlPath: "https://github.com/Crois0509")
     ]
     
     var collectionView: UICollectionView!
@@ -47,9 +112,10 @@ class MemberViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
-        let name = members[indexPath.row].0
-        let tmi = members[indexPath.row].1
-        cell.memberImageView.image = UIImage(named: name)
+        let name = members[indexPath.row].getEnName()
+        let tmi = members[indexPath.row].getTmi()
+        let imgAssetName = members[indexPath.row].getImgAssetName()
+        cell.memberImageView.image = UIImage(named: imgAssetName)
         cell.memberNameLabel.text = name
         cell.memberTmiLabel.text = tmi
         return cell
